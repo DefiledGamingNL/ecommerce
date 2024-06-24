9788ac2d11f99f7c5268710014bfe6c5c8f9618a
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-06-24 22:23:29 +0200

    removed some styling from the component.

--- resources/js/Components/Content.vue
+++ resources/js/Components/Content.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div class="p-4 bg-gray-50 rounded-lg mt-14">
             <slot />
         </div>
     </div>
