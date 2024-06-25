52b1dc89b75e5e913e139329fe7d72e2f3483900
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-06-25 13:41:42 +0200

    changed PostController to act as boilerplate for now.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,32 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PostController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
      */
-    public function __invoke(Request $request)
+    public function index()
     {
-        return Inertia::render('Posts/Index', [
-            'filters' => $request->all('search', 'trashed'),
-            'posts' => Post::orderBy('created_at', 'desc')
-                ->paginate()
-                ->transform(function ($post) {
-                    return [
-                        'id' => $post->id,
-                        'title' => $post->title,
-                        'slug' => $post->slug,
-                        'author' => $post->author->name,
-                        'category' => $post->category->only('id', 'title'),
-                        'created_at' => $post->created_at,
-                        'updated_at' => $post->updated_at,
-                    ];
-                }),
-        ]);
+        return Inertia::render('Posts/Index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
