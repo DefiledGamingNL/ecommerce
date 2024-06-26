3145b62d959edcfefdeb07645957e21ef34cd8e6
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-06-26 22:00:05 +0200

    added factory and seeder for posts and users

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true),
+            'slug' => $this->faker->slug,
+            'meta_description' => $this->faker->sentence,
+            'meta_keywords' => $this->faker->sentence,
+            'user_id' => User::factory(),
+            // Add more fields as needed
+        ];
+    }
+}
