c1732b89ef38cf2ed8c8d014fae545d4951ebc84
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-06-28 21:02:14 +0200

    renamed Welcome to Home

---
+++ resources/js/Pages/Homepage/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import Topbar from "@/Components/Topbar.vue";
+import HeaderBanner from "@/Components/HeaderBanner.vue";
+import Header from "@/Components/Header.vue";
+</script>
+
+<template>
+<Header>
+    <Topbar />
+    <HeaderBanner />
+</Header>
+</template>
