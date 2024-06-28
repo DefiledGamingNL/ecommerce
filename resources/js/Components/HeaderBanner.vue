ee498e5b0c34a447654ababa7a787e0dcfbe30fd
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-06-28 21:04:40 +0200

    created HeaderBanner for our featured product banner

---
+++ resources/js/Components/HeaderBanner.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+
+<template>
+    <section id="banner">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between items-center relative">
+                <div class="relative z-10">
+                    <h1 class="relative z-10 text-4xl font-bold text-gray-800">Welcome to our website</h1>
+                    <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, alias.</p>
+                </div>
+                    <img src="https://via.placeholder.com/4000x600" alt="banner" class="absolute top-0">
+            </div>
+        </div>
+    </section>
+</template>
