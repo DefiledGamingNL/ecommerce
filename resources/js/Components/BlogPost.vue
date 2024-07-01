6c365e3f285775e7eb1d2f8fd0e882d9cff13873
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-01 19:13:14 +0200

    created Blogpost card for homepage

---
+++ resources/js/Components/BlogPost.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {Link} from '@inertiajs/vue3';
+defineProps({
+    post: Object
+})
+</script>
+
+<template>
+    <section id="blog-post">
+        <div class="max-w-7xl mx-auto bg-gray-100 p-10">
+            <h2 class="text-4xl font-bold">{{post.title}}</h2>
+            <div class="mt-4">
+                <div class="mt-4">
+                    <p class="text-gray-600">{{post.content}}</p>
+                    <div class="mt-4">
+                        <Link :href="post.slug" class="text-blue-500">Read more</Link>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </section>
+</template>
