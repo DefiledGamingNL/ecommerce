d7e22b3d3bc91649813aa30bced708d0733c812f
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-01 19:13:44 +0200

    some css changes for max height

--- resources/js/Components/HeaderBanner.vue
+++ resources/js/Components/HeaderBanner.vue
@@ -3,14 +3,14 @@
 
 
 <template>
-    <section id="banner">
-        <div class="max-w-7xl mx-auto">
+    <section id="banner" class="relative">
+        <div class="max-w-7xl mx-auto h-[300px]">
             <div class="flex justify-between items-center relative">
                 <div class="relative z-10">
                     <h1 class="relative z-10 text-4xl font-bold text-gray-800">Welcome to our website</h1>
                     <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, alias.</p>
                 </div>
-                    <img src="https://via.placeholder.com/4000x600" alt="banner" class="absolute top-0">
+                    <img src="https://via.placeholder.com/4000x900" alt="banner" class="absolute top-0">
             </div>
         </div>
     </section>
