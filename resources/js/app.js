934b0a22da235e5ce31e78c17f920844ecc4cf4c
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:37:52 +0200

    added tinymce and sweetalerts

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,6 +22,7 @@ createInertiaApp({
 
         return vueApp
             .use(plugin)
+            .use(VueSweetalert2)
             .use(ZiggyVue)
             .mount(el);
     },
