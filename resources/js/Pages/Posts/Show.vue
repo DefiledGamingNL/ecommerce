c19e026215c2be9023fa07c8d027ceb06adcd873
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:40:00 +0200

    wrapped individual post page in the guestlayout component

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
 defineProps({
     post: Object,
 })
@@ -8,15 +10,17 @@ defineProps({
 
 <template>
 
-        <div class="max-w-2xl mx-auto py-4 px-2">
-            <div class="flex flex-col">
-                <div class="heading">
-                    <h1 class="text-7xl font-bold text-gray-800 dark:text-white">{{ post.title }}</h1>
-                </div>
-                <p class="text-gray-500 dark:text-gray-400">{{$formatDate(post.created_at)}}</p>
-                <div class="mt-5">
-                    <p class="text-gray-800 dark:text-white">{{ post.content }}</p>
+        <GuestLayout>
+            <div class="max-w-2xl mx-auto py-4 px-2">
+                <div class="flex flex-col">
+                    <div class="heading">
+                        <h1 class="text-7xl font-bold text-gray-800 dark:text-white">{{ post.title }}</h1>
+                    </div>
+                    <p class="text-gray-500 dark:text-gray-400">{{$formatDate(post.created_at)}}</p>
+                    <div class="mt-5" >
+                        <div id="content" v-html="post.content"></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </GuestLayout>
 </template>
