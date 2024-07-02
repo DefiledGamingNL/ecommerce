2bcb410cc645f8b4a038db9da696fb57e46d58b0
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:41:51 +0200

    added the hasmany relation for pages to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,4 +73,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function pages(): HasMany
+    {
+        return $this->hasMany(Page::class);
+    }
 }
