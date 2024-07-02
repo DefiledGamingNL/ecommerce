550614ef1b5d9c657601250b381a53307394b81c
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:38:26 +0200

    added wysiwyg editor TinyMCE

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -21,7 +21,22 @@
                     </div>
                     <div class="md:col-span-2">
                         <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
-                        <textarea id="content" v-model="form.content" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Content" required ></textarea>
+                        <Editor
+                            api-key="vapv8a9vycoqgyogceu2h0s6xjnne7j691mou628viibmv7c"
+                            v-model="form.content"
+                            :init="{
+        toolbar_mode: 'sliding',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+                        }"
+                        initial-value="Welcome to TinyMCE!"
+                        />
                     </div>
                 </div>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
@@ -31,6 +46,7 @@
 </template>
 
 <script setup>
+import Editor from '@tinymce/tinymce-vue'
 import { ref } from 'vue'
 import { router } from '@inertiajs/vue3'
 import AppLayout from "@/Layouts/AppLayout.vue";
