e8de334d90f54dfef51c8ef85b7e13edbbc45656
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:40:47 +0200

    changed links to go to route instead of just href menuItem.url

--- resources/js/Components/BlogPost.vue
+++ resources/js/Components/BlogPost.vue
@@ -2,7 +2,7 @@
 
 import {Link} from '@inertiajs/vue3';
 defineProps({
-    post: Object
+    post: Array
 })
 </script>
 
@@ -12,9 +12,9 @@ defineProps({
             <h2 class="text-4xl font-bold">{{post.title}}</h2>
             <div class="mt-4">
                 <div class="mt-4">
-                    <p class="text-gray-600">{{post.content}}</p>
+                    <div class="text-gray-600" v-html="post.content"></div>
                     <div class="mt-4">
-                        <Link :href="post.slug" class="text-blue-500">Read more</Link>
+                        <Link :href="route('posts.show', post.slug)" class="text-blue-500">Read more</Link>
                         </div>
                     </div>
                 </div>
