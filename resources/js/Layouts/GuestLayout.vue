6d93671a80a9e401c1af8e74d60dc694e7b22480
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-02 21:38:51 +0200

    added guest layout for frontend

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from "@/Components/Header.vue";
+import MainElement from "@/Components/MainElement.vue";
+import Topbar from "@/Components/Topbar.vue";
+import HeaderBanner from "@/Components/HeaderBanner.vue";
+</script>
+
+
+<template>
+    <Header>
+        <Topbar />
+        <slot name="topbar" />
+
+    </Header>
+    <MainElement>
+        <slot />
+    </MainElement>
+
+</template>
