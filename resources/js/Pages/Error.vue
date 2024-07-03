5e2b57247b0d6efd0b15d859f794020d17a23901
Danny <79605355+DefiledGamingNL@users.noreply.github.com>
2024-07-03 20:25:35 +0200

    Added custom error page.

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
+const props = defineProps({ status: Number })
+
+const title = computed(() => {
+    return {
+        503: '503: Service Unavailable',
+        500: '500: Server Error',
+        404: '404: Page Not Found',
+        403: '403: Forbidden',
+    }[props.status]
+})
+
+const description = computed(() => {
+    return {
+        503: 'Sorry, we are doing some maintenance. Please check back soon.',
+        500: 'Whoops, something went wrong on our servers.',
+        404: 'Sorry, This is not the page you are looking for.',
+        403: 'Sorry, you are forbidden from accessing this page.',
+    }[props.status]
+})
+</script>
+
+<template>
+    <GuestLayout>
+        <div class="max-w-7xl mx-auto">
+            <h1 class="text-7xl text-center m-32">{{ title }}</h1>
+            <div class="text-5xl text-center">{{ description }}</div>
+        </div>
+    </GuestLayout>
+</template>
